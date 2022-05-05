d2ade1cf9501df718451b945572e77cd83ee4198
Hannah Chi <cremi468@gmail.com>
2022-05-06 00:21:31 +0800

    Initial Commit

---
+++ MonsterDoll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MonsterDoll
+//
+//  Created by Hsiao-Han Chi on 2022/5/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
